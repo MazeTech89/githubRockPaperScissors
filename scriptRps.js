99d83839ba02b9f86fb0e52c76a54d53eefc2858
MazeTech89 <74939600+MazeTech89@users.noreply.github.com>
2021-07-28 06:09:01 +0100

    first commit

---
+++ scriptRps.js
@@ -0,0 +1,65 @@
+const selectionButtons = documnet.querySelectorAll('[data-selection]')
+const finalColumn =document.querySelector('[data-final-column]')
+const SELECTIONS = [
+{
+	name: 'rock',
+	emoji: '✊🏽',
+	beats: 'scissors'
+
+},
+
+{
+	name: 'paper',
+	emoji: '🤚🏽',
+	beats: 'rock'
+
+},
+
+{
+	name: 'scissors',
+	emoji: '✌🏽',
+	beats: 'paper'
+
+}
+
+]
+selectionButtons.forEach(selectionButton => {
+	selectionButton.addEventListener('click', e => {
+		const selectionName = selectionButton.dataset.selection
+		const selection = SELECTIONS.find(selection => selection.name === selectionName)
+		makeSelection(selectionName)
+
+	})
+	
+});
+
+/*Automate what the computer selects at randon*/
+function makeSelection(selection) {
+	const computerSelection = randomSelection()
+	const yourWinner = isWinner(selection, computerSelection)
+	const computerWinner = isWinner(computerSelection, selection)
+	
+	addSelectionResult(computerSelection, computerWinner)
+	addSelectionResult(selection, yourWinner)
+
+}
+
+function addSelectionResult(selection, winner) {
+	const div = document.createElement('div')
+	div.innerText=selection.emoji
+	div.classList.add('result.selection')
+	if(winner) div.classList.add('winner')
+	finalColumn.after(div)
+
+}
+
+function isWinner(selection, opponentSelection) {
+	return selection.beats === opponentSelection.name
+
+}
+
+function randomSelection() {
+	const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+	return SELECTIONS[randomIndex]
+
+}
